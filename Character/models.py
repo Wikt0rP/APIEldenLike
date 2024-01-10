6cbb474b874e2d055001539421a7551dfaec913d
ASzazX <prokopiszynwiktor@gmail.com>
2024-01-10 21:18:38 +0100

    adding models (character and stats)

--- Character/models.py
+++ Character/models.py
@@ -7,4 +7,4 @@ class Character(models.Model):
     characterName = models.CharField(max_length=100, null=False, blank=False)
     level = models.IntegerField(null=False, blank=False)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=False)
-    stats
+
