42709d6063998f81131603d3ed5a659f4abc47da
ASzazX <prokopiszynwiktor@gmail.com>
2024-01-10 21:38:57 +0100

    adding models (character and stats)

--- Character/models.py
+++ Character/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+from Stats.models import Stats
 
 
 class Character(models.Model):
     characterName = models.CharField(max_length=100, null=False, blank=False)
     level = models.IntegerField(null=False, blank=False)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=False)
+    stats = models.OneToOneField(Stats, on_delete=models.CASCADE, null=True, blank=False)
 
