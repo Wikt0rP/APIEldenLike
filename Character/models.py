3dee818e4181264d56c7cefc0aece34708fde507
ASzazX <prokopiszynwiktor@gmail.com>
2024-01-10 21:17:58 +0100

    gitignore added

--- Character/models.py
+++ Character/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Character(models.Model):
+    characterName = models.CharField(max_length=100, null=False, blank=False)
+    level = models.IntegerField(null=False, blank=False)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=False)
+    stats
