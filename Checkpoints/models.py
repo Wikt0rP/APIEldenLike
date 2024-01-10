b9ee53717ee0fb2667ab1d13a7ad81d0aeed5273
ASzazX <prokopiszynwiktor@gmail.com>
2024-01-10 22:56:56 +0100

    All models ready

--- Checkpoints/models.py
+++ Checkpoints/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Checkpoints(models.Model):
+    checkpointName = models.CharField(max_length=100)
+    checkpointX = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    checkpointY = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    checkpointZ = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
--- Inventory/models.py
+++ Inventory/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from Character.models import Character
+from Item.models import Item
 
-# Create your models here.
+
+class Inventory(models.Model):
+    character = models.OneToOneField(Character, on_delete=models.CASCADE)
+    itemInventory = models.ManyToManyField(Item, blank=True, related_name='itemInventory')
+    weaponInventory = models.ManyToManyField(Item, blank=True, related_name='weaponInventory')
