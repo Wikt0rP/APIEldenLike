c5339bacb9df9eebb080dea668d918801ce5bb8f
ASzazX <prokopiszynwiktor@gmail.com>
2024-01-10 23:13:57 +0100

    - money

--- MapLocation/models.py
+++ MapLocation/models.py
@@ -4,5 +4,5 @@ from Checkpoints.models import Checkpoints
 
 class MapLocation(models.Model):
     locationName = models.CharField(max_length=100)
-    checkpoint = models.ManyToOneRel(Checkpoints, on_delete=models.CASCADE, related_name='checkpoints')
+    checkpoint = models.ForeignKey(Checkpoints, on_delete=models.CASCADE, related_name='checkpoints')
     description = models.TextField(null=True, blank=True)
